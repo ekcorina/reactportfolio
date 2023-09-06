f510e9c50dff0a0e87557dbc58661a65c4ff7f48
ekcorina <ekcorina5@gmail.com>
2023-09-06 10:46:00 -0700

    Created header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
 
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header/Header';
 
 
 
@@ -13,7 +13,9 @@ function App() {
 
   return (
     <>
+    <Header/>
 <Navbar/>
+
     </>
   );
 }
